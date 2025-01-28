eb6c2a2857bfaa63051878b61b83398cc3e1db29
Degra02 <filippodegrandi02@gmail.com>
2025-01-28 01:45:38 +0100

    random values concerns. oh well

--- src/utils.c
+++ src/utils.c
@@ -55,9 +55,18 @@ void generate_complete_graph(const int V, const char *filename) {
   igraph_integer_t V_igraph = V;
   igraph_full(&graph, V_igraph, IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
 
+  // NOTE: is this better than get_random() or rand()?
+  // the answer is left to the whoever wants to lose time
+  FILE *random = fopen("/dev/urandom", "r");
+  if (random == NULL) {
+    fprintf(stderr, "Error opening /dev/urandom.\n");
+    exit(1);
+  }
+
+  unsigned int random_value;
+
   // print graph to file
   fprintf(file, "%d %d\n", V, V * (V - 1) / 2);
-
 
   igraph_integer_t from, to;
   igraph_vector_int_t edges;
@@ -65,7 +74,7 @@ void generate_complete_graph(const int V, const char *filename) {
   igraph_vector_int_init(&edges, E);
   igraph_get_edgelist(&graph, &edges, 0);
   for (int i = 0; i < E; i++) {
-    unsigned int random_value = get_random();
+    fread(&random_value, sizeof(unsigned int), 1, random);
     from = VECTOR(edges)[2 * i];
     to = VECTOR(edges)[2 * i + 1];
     fprintf(file, "%d %d %d\n", (int)from, (int)to, random_value % MAX_WEIGHT + 1);
